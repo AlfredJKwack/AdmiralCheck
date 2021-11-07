3fa4007f63850e349dcf093d01479ca9c79b1ab0
AlfredJKwack <alfred_j_kwack@badsoda.com>
2021-11-07 15:25:33 +0100

    Upgrade to RollingCurl, refactor and todo done
    Swiched to PEPSIASAS/RollingCurl library as it provides some extra goodness in callback functions.
    
    Refactored the code for legibility.
    
    Now also generate a decent output for a PR to prune dead domains from jkrejcha/AdmiraList.
    
    Todo:
    - update README.md
    - generate an abstract analsysis file

--- main.php
+++ main.php
@@ -1,70 +1,117 @@
 <?php
 
 // more efficient implementation of curl_multi()
-// see https://github.com/LionsAd/rolling-curl
-require 'rolling-curl/RollingCurl.php';
-require 'rolling-curl/RollingCurlGroup.php';
+// see https://github.com/PEPSIASAS/RollingCurl
+require 'RollingCurl/src/RollingCurlService/RollingCurl.php';
+require 'RollingCurl/src/RollingCurlService/RollingCurlRequest.php';
 
-class MyInfoCollector {
-    private $rc;
-	private $ob_file;
-	private $strout; 
+$input_file = "AdmiraList/AdmiraList.txt";
+$output_analysis = "AlCheckOut_analysis.txt";
+$output_pr_dead = "AlCheckOut_pr_dead.txt";
+$output_unique_hashes = "AlCheckOut_pr_dead.txt";
 
-    function __construct(){
+/**
+ * Creates an output file where all dead domains are removed.
+ * This is useful to generate a PR at some point
+ * @param  String $output_file Path to output file. I will be overwritten
+ * @param  Array  $data        Input data in the format used throughout
+ * @return none
+ */
+function generateDeadPrFile($output_file, $data){
 
-    	$output_file = "results.txt";
-        $this->rc = new RollingCurl(array($this, 'processPage'));
-        $this->ob_file = fopen($output_file, "w") or die("Unable to open file!");
+	function liveDomain($theDomain){
+		// remove all domains where http_code was 0
+		return !($theDomain['return_code'] === 0);
+	}
 
-    }
+	$filteredArray = array_keys(array_filter($data, "liveDomain"));
+	sort($filteredArray);
 
-    function processPage($response, $info){
+	file_put_contents($output_file, implode(PHP_EOL, $filteredArray));
+};
 
-    	$this->strout .= $info['http_code']."\t";
+/**
+ * Creates a tab delimited output file ready for further analysis in your 
+ * favorite data manipulation tool.
+ * @param  String $output_file Path to output file. I will be overwritten
+ * @param  [type] $data        Input data in the format used throughout.
+ * @return none
+ */
+function generateAnalysisFile($output_file, $data){
 
+	$output = "";
+	foreach ($data as $k => $v){
+		$output .= $v['return_code']."\t";
+		$output .= $v['txt_match']."\t";
+		$output .= $v['md5']."\t";
+		$output .= $k."\n";
+	}
 
-    	// find our nice Admiral string
-    	$lookfor = "This domain is used by digital publishers to control access to copyrighted content ";
-    	$string = str_replace("\r\n", "\n", $response); // windows -> unix
-		$string = str_replace("\r", "\n", $string);   // remaining -> unix     
+	$ob_file = fopen($output_file, "w") or die("Unable to open file!");
+	fwrite($ob_file, $output);
+	fclose($ob_file);	
+};
 
-		if (strpos($string, $lookfor) !== false) {
-			
-			$this->strout .= "txt"."\t";
+/**
+ * Takes an array of domain names as input and generates a new 
+ * multi-dimensional array with the format as follows.
+ *
+ *   ["theInputDomainName"]=>
+ *     array(4) {
+ *         ["url"]=> "https://advertisementafterthought.com/"
+ *         ["return_code"]=> ''
+ *         ["txt_match"]=> ''
+ *         ["md5"]=> ''
+ *     }
+ * 
+ * @param  array $inputArr An array of domain names
+ * @return array           The array as described 
+ */
+function generateDataArray($inputArr){
+	$tmpArr = array();
+	array_walk($inputArr, function ($value, $key) use (&$tmpArr) {
+		$tmpArr[$value] = array(
+			'url' => "https://".$value."/",		// url we will query
+			'return_code' => '',				// http_code returned by query
+			'txt_match' => '',					// is there a magic text match?
+			'md5' => ''							// an md5 hash of the webpage
+		);
+	});
+	return $tmpArr;
+};
 
-		} else {
+// transform input file into a useful array.
+$theInput = file($input_file, FILE_IGNORE_NEW_LINES);
+$domains = generateDataArray($theInput);
 
-			$this->strout .= "n/a"."\t";
-		}
+$rollingCurl = new \RollingCurlService\RollingCurl();
 
-		$this->strout .= md5($string)."\t";
-
-		$this->strout .= $info['url']."\r";
-
-    }
-
-    function run($input_file){
-
-	    function domainToUrl($domain){
-	    	return ("https://".$domain."/");
-	    }
-
-    	$urls = file($input_file, FILE_IGNORE_NEW_LINES);
-    	$urls = array_map('domainToUrl', $urls);
-
-        foreach ($urls as $url){
-
-            $request = new RollingCurlRequest($url);
-            $this->rc->add($request);
-
-        }
-        $this->rc->execute();
-
-		fwrite($this->ob_file, $this->strout);
-		fclose($this->ob_file);        
-    }
+// queue up each of the domains for Curl requests to be generated
+foreach ($domains as $key => $domain) {
+    $request = new \RollingCurlService\RollingCurlRequest($domain["url"]);
+    $request->setAttributes(['requestId'=>$key]); // for callback goodness
+    $rollingCurl->addRequest($request);
 }
 
-$collector = new MyInfoCollector();
-$collector->run("AdmiraList/AdmiraList.txt");
+// exectue the Curl request and update the data array
+$rollingCurl->execute(function ($output, $info, $request) use (&$domains){
+
+	$lookfor = "This domain is used by digital publishers to control access to copyrighted content ";
+
+    $requestAttributes = $request->getAttributes(); // callback goodness
+    $domains[$requestAttributes['requestId']]["return_code"] = $info['http_code'];
+    $domains[$requestAttributes['requestId']]["md5"] = md5($output);
+    $domains[$requestAttributes['requestId']]["url"] = $info['url'];
+
+	$string = str_replace(array("\r","\n","\t"), "", $output);
+	if (strpos($string, $lookfor) !== false) {
+		$domains[$requestAttributes['requestId']]["txt_match"] =  "txt";
+	} else {
+		$domains[$requestAttributes['requestId']]["txt_match"] = "n/a";
+	} 
+
+});
+generateDeadPrFile($output_pr_dead, $domains);
+generateAnalysisFile($output_analysis, $domains);
+
 ?>
