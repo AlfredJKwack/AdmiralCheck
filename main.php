cd9e5cdccb258dad95e18ab6e10ca8a300685f2e
AlfredJKwack <alfred_j_kwack@badsoda.com>
2021-11-07 21:05:49 +0100

    Generate report of unique sites & more
    Create a new file listing only unique hashes. It will list one dead domain while there may be more in the data. The point is to easily identify live domains that do not look like Admiral. Also produce a json of the entire data set for easy manipulation.

--- main.php
+++ main.php
@@ -5,10 +5,16 @@
 require 'RollingCurl/src/RollingCurlService/RollingCurl.php';
 require 'RollingCurl/src/RollingCurlService/RollingCurlRequest.php';
 
-$input_file = "AdmiraList/AdmiraList.txt";
+$input_file = "testData/PR.txt";
 $output_analysis = "AlCheckOut_analysis.txt";
 $output_pr_dead = "AlCheckOut_pr_dead.txt";
-$output_unique_hashes = "AlCheckOut_pr_dead.txt";
+$output_unique_hashes = "AlCheckOut_Abstracts.txt";
+$output_json = "AlCheckOut.json";
+
+// a string of text we know is present on the Admiral sites
+$lookfor = "This domain is used by digital publishers to control access to copyrighted content ";
+
+// $input_file = "testData/testList.txt";
 
 /**
  * Creates an output file where all dead domains are removed.
@@ -80,6 +86,45 @@ function generateDataArray($inputArr){
 	return $tmpArr;
 };
 
+/**
+ * Takes a multi-dimentional input array and returns a new array without 
+ * duplicate values for a given key value. The first equal element will be 
+ * retained,
+ *
+ * See: https://www.php.net/manual/en/function.array-unique.php#116302
+ * 
+ * @param  Array  $multiArr The array that we wish to reduce from
+ * @param  String $key      The key we will make unique
+ * @return Array            The array containing only unique keys
+ */
+function array_multidim_unique($multiArr, $key) {
+    $temp_array = array();
+    $i = 0;
+    $key_array = array();
+   
+    foreach($multiArr as $arrSrcKey => $val) {
+        if (!in_array($val[$key], $key_array)) {
+            $key_array[$i] = $val[$key];
+            $temp_array[$arrSrcKey] = $val;
+        }
+        $i++;
+    }
+    return $temp_array;
+}
+
+/**
+ * Genrates a file containing only entries where the hash is unique
+ * @param  String $output_file Path to output file. It will be overwritten
+ * @param  Array  $data        Input data in the format used throughout
+ * @return none
+ */
+function generateAbstractsFile($output_file, $data){
+
+	$output = array_multidim_unique($data,'md5');
+	generateAnalysisFile($output_file, $output);
+
+}
+
 // transform input file into a useful array.
 $theInput = file($input_file, FILE_IGNORE_NEW_LINES);
 $domains = generateDataArray($theInput);
@@ -94,9 +139,7 @@ foreach ($domains as $key => $domain) {
 }
 
 // exectue the Curl request and update the data array
-$rollingCurl->execute(function ($output, $info, $request) use (&$domains){
-
-	$lookfor = "This domain is used by digital publishers to control access to copyrighted content ";
+$rollingCurl->execute(function ($output, $info, $request) use (&$domains, $lookfor){
 
 	// clean up the output a little
 	$cleanOutput = strip_tags($output);
@@ -113,7 +156,8 @@ $rollingCurl->execute(function ($output, $info, $request) use (&$domains){
 	}
 
 });
+file_put_contents($output_json,json_encode($domains));
 generateDeadPrFile($output_pr_dead, $domains);
 generateAnalysisFile($output_analysis, $domains);
-
+generateAbstractsFile($output_unique_hashes,$domains);
 ?>
